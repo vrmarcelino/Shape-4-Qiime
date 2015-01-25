3fa5b03b6e3127c5a7faa7bbd9c4d7e8b08780bc
Vanessa Rossetto Marcelino <vrmarcelino@gmail.com>
2015-01-25 17:55:38 +1100

    pipe 1 ok

--- 00_Pipe_1.sh
+++ 00_Pipe_1.sh
@@ -56,6 +56,7 @@ for file in 02_Separated_genes/18S/*.fastq; do convert_fastaqual_fastq.py -f $fi
 for file in 02_Separated_genes/UPA/*.fastq; do convert_fastaqual_fastq.py -f $file -c fastq_to_fastaqual -o 03_Fasta_Qual/UPA/; done >>screen.out 2>> screen.err
 for file in 02_Separated_genes/tufA/*.fastq; do convert_fastaqual_fastq.py -f $file -c fastq_to_fastaqual -o 03_Fasta_Qual/tufA/; done >>screen.out 2>> screen.err
 
+source ~/.bash_profile
 
 ### Merge fasta and qual - need samples_list.csv with indexes
 echo "merging..."
@@ -89,4 +90,4 @@ merge_qual.py 03_Fasta_Qual/tufA/*.qual
 mv all_records.* 04_All_records/tufA
 mv map.txt 04_All_records/tufA
 
-
+echo "End of pipe 1"
